a3669108a62a9291e30bee3b318a735bcdc6696b
Muhammad ibrahim <ibgame014@gmail.com>
2021-11-25 15:05:59 +0500

    firebase data save

---
+++ App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Text, TextInput, View } from 'react-native'
+import firestore from '@react-native-firebase/firestore';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      age: '',
+      addres:'',
+      phone:'',
+      username:'',
+      password:''
+    }
+  }
+  adddata = () => {
+    firestore()
+      .collection('apnakam ')
+      .add({
+        name: this.state.name,
+        age: this.state.age,
+        addres:this.state.addres,
+        phone:this.state.phone,
+        username:this.state.username,
+        password:this.state.password,
+      })
+      .then(() => {
+        alert("data added")
+      });
+  }
+  addName = text => { this.setState({ name: text }) }
+  addage = age1 => { this.setState({ age: age1 }) }
+  addadress = text => { this.setState({ addadress: text }) }
+  addphone = text => { this.setState({ phone: text }) }
+  addusername = text => { this.setState({ username: text }) }
+  addpasword = text => { this.setState({ password: text }) }
+  render() {
+    return (
+      <View>
+        <TextInput placeholder='enter name' value={this.state.name} onChangeText={text => this.addName(text)}
+         style={{ borderWidth: 1, textAlign: 'center', margin: 10 }} />
+
+        <TextInput placeholder='enter age' value={this.state.age} onChangeText={age1 => { this.addage(age1) }}
+         style={{ borderWidth: 1, textAlign: 'center', margin: 10 }} />
+
+        <TextInput placeholder='enter address' value={this.state.addres} onChangeText={text => { this.addadress(text) }}
+         style={{ borderWidth: 1, textAlign: 'center', margin: 10 }} />
+
+        <TextInput placeholder='enter phone' value={this.state.phone} onChangeText={phone => { this.addphone(phone) }}
+         style={{ borderWidth: 1, textAlign: 'center', margin: 10 }} />
+
+        <TextInput placeholder='enter username' value={this.state.username} onChangeText={age1 => { this.addusername(age1) }}
+         style={{ borderWidth: 1, textAlign: 'center', margin: 10 }} />
+
+        <TextInput placeholder='enter password' value={this.state.password} onChangeText={age1 => { this.addpasword(age1) }} 
+        style={{ borderWidth: 1, textAlign: 'center', margin: 10 }} />
+        <Text onPress={this.adddata} style=
+          {{
+            textAlign: 'center', fontSize: 23, margin: 20, borderWidth: 2
+          }}> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+
+
+
+
+
+
